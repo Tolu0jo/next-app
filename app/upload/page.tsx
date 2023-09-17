1bdd2c16b92103affad9c885d2b33f151a212f92
Teetony <decagon@Fibonnacci.local>
2023-09-17 17:26:16 +0100

    upload images

---
+++ app/upload/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+import { useState } from "react";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
+  return (
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="img" />
+      )}
+      <CldUploadWidget
+        uploadPreset="Teetony"
+        options={{
+          sources: [
+            "local",
+            "image_search",
+            "google_drive",
+            "instagram",
+            "camera",
+          ],
+          multiple: false,
+        }}
+        onUpload={(result, widget) => {
+          console.log(result);
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => {
+          function handleOnClick(e: any) {
+            e.preventDefault();
+            open();
+          }
+          return (
+            <button className="btn btn-primary" onClick={handleOnClick}>
+              Upload an Image
+            </button>
+          );
+        }}
+      </CldUploadWidget>{" "}
+    </>
+  );
+};
+export default UploadPage;
